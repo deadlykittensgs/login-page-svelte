f2b246772a9ab16a6c928cde4d93fb5af1527d51
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-07-01 11:49:17 -0500

    adding to git

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,205 @@
+<script>
+  // use bind to set value
+  let email = "";
+  let password = "";
+  let confirmPassword = "";
+  let error = false;
+  let register = false;
+
+  function handleAuthenticate() {
+    if (!email || !password || (register && !confirmPassword)) {
+      error = true;
+      return;
+    }
+  }
+  function handleRegister() {
+    register = !register;
+  }
+</script>
+
+<div class="authContainer">
+  <form>
+    <h1>{register ? "Register" : "Login"}</h1>
+    <!-- conditional render if error = true  -->
+    {#if error}
+      <p class="error">The information you have entered is not correct</p>
+    {/if}
+    <label>
+      <p class={email ? "above" : "center"}>Email</p>
+      <input bind:value={email} type="email" placeholder="Email" />
+    </label>
+    <label>
+      <p class={password ? "above" : "center"}>Password</p>
+      <input bind:value={password} type="password" placeholder="Password" />
+    </label>
+    {#if register}
+      <label>
+        <p class={confirmPassword ? "above" : "center"}>Confirm Password</p>
+        <input
+          bind:value={confirmPassword}
+          type="password"
+          placeholder="Confirm Password"
+        />
+      </label>
+    {/if}
+    <button type="button">Submit</button>
+  </form>
+  <div class="options">
+    <p>Or</p>
+    {#if register}
+      <div>
+        <p>Already have an Account?</p>
+        <p on:click={handleRegister} on:keydown={() => {}}>Login</p>
+      </div>
+    {:else}
+      <div>
+        <p>Don't have an account??</p>
+        <p on:click={handleRegister} on:keydown={() => {}}>Register</p>
+      </div>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .authContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    padding: 24px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+  }
+
+  form,
+  .options {
+    width: 400px;
+    max-width: 100%;
+    margin: 0 auto;
+  }
+
+  form input {
+    width: 100%;
+    border: none;
+    background: transparent;
+    color: rgb(255, 255, 255);
+    padding: 14px;
+  }
+  form input:focus {
+    border: none;
+    outline: none;
+  }
+
+  form label:focus-within {
+    border-color: blue;
+  }
+
+  form label {
+    position: relative;
+    border: 1px solid navy;
+    border-radius: 5px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+  }
+
+  form button {
+    background: blue;
+    color: white;
+    border: none;
+    padding: 14px 0;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1.2rem;
+  }
+
+  form button:hover {
+    background-color: cyan;
+  }
+
+  .above,
+  .center {
+    position: absolute;
+    transform: translateY(-50%);
+    pointer-events: none;
+    color: white;
+    border-radius: 4px;
+    padding: 0 6px;
+    font-size: 0.8rem;
+  }
+
+  .above {
+    top: 0;
+    left: 24px;
+    background: navy;
+    border: 1px solid blue;
+    font-size: 0.7rem;
+  }
+  .center {
+    top: 50%;
+    left: 6px;
+    border: 1px solid transparent;
+    opacity: 0;
+  }
+
+  .error {
+    color: coral;
+    font-size: 0.9rem;
+  }
+
+  .options {
+    padding: 14px 0;
+    overflow: hidden;
+    font-size: 0.9rem;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  }
+
+  /* direct decedent  */
+  .options > p {
+    position: relative;
+    text-align: center;
+    width: fit-content;
+    margin: 0 auto;
+    padding: 0 8px;
+  }
+
+  .options > p::after,
+  .options > p::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100vw;
+    height: 1.5px;
+    background: white;
+  }
+
+  .options > p::after {
+    right: 100%;
+  }
+
+  .options > p::before {
+    left: 100%;
+  }
+
+  .options div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    justify-content: center;
+  }
+
+  .options div p:last-of-type {
+    color: cyan;
+    cursor: pointer;
+  }
+</style>
